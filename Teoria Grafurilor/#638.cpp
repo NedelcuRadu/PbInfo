ebbf3c6ec24b6f40102cbdc1183dee15c153aae1
ShumaherK <43296487+ShumaherK@users.noreply.github.com>
2019-04-25 13:26:18 +0300

    Create #638.cpp

---
+++ Teoria Grafurilor/#638.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+ifstream in("nivele.in");
+ofstream out("nivele.out");
+
+int N, K;
+int A[101];
+int root;
+int viz[101];
+
+void citire() {
+    in >> N;
+    for(int i = 1; i <= N; i++) {
+        in >> A[i];
+        if(A[i] == 0)
+            root = i;
+    }
+    in >> K;
+}
+
+void DFS(int nod, int nivel) {
+    viz[nod] = nivel;
+    for(int i = 1; i <= N; i++)
+        if(A[i] == nod && viz[i] == 0)
+            DFS(i, nivel + 1);
+
+}
+int main() {
+    citire();
+    int x;
+    DFS(root, 1);
+    for(int i = 1; i <= K; i++) {
+        in >> x;
+        out << viz[x] << '\n';
+    }
+
+    return 0;
+}
