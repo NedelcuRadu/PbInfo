50f08cd988c7b93a4574f5e4af28275cd36e36a8
ShumaherK <43296487+ShumaherK@users.noreply.github.com>
2019-04-25 13:27:12 +0300

    Update #638.cpp

--- Teoria Grafurilor/#638.cpp
+++ Teoria Grafurilor/#638.cpp
@@ -14,19 +14,19 @@ void citire() {
     in >> N;
     for(int i = 1; i <= N; i++) {
         in >> A[i];
-        if(A[i] == 0)
+        if(A[i] == 0) //Tinem minte radacina pt DFS
             root = i;
     }
     in >> K;
 }
 
 void DFS(int nod, int nivel) {
-    viz[nod] = nivel;
+    viz[nod] = nivel; //Marcam nivelul
     for(int i = 1; i <= N; i++)
-        if(A[i] == nod && viz[i] == 0)
+        if(A[i] == nod && viz[i] == 0) //Daca nodul e fiu si nu e vizitat
             DFS(i, nivel + 1);
-
 }
+
 int main() {
     citire();
     int x;
