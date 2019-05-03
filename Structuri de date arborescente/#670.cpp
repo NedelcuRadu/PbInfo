846abe2ad2bb8356778b7bd240feaccd2bf3fb73
ShumaherK <43296487+ShumaherK@users.noreply.github.com>
2019-05-04 01:35:43 +0300

    Create #670.cpp

---
+++ Structuri de date arborescente/#670.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#define MAX 1010
+using namespace std;
+
+ifstream in("preordine.in");
+ofstream out("preordine.out");
+
+typedef struct nod {
+    int key;
+    nod *st, *dr;
+}*pNod;
+
+pNod v[MAX];
+int N;
+vector<int> A;
+
+void add(pNod &dest, int akey, pNod ast, pNod adr) {
+    dest->st = ast;
+    dest->dr = adr;
+    dest->key = akey;
+}
+
+void citire() {
+    int x, y, z;
+    in >> N;
+    for(int i = 1; i <= N; i++) {
+        v[i] = new nod;
+    }
+    for(int i = 1; i <= N; i++) {
+        in >> x >> y >> z;
+        add(v[i], x, v[y], v[z]);
+    }
+}
+
+void preordine(pNod root) {
+    A.push_back(root->key);
+
+    if(root->st != NULL)
+        preordine(root->st);
+
+    if(root->dr != NULL)
+        preordine(root->dr);
+}
+
+void afis() {
+    for(int i = 0; i < A.size(); i++)
+        out << A[i] << " ";
+}
+
+int main() {
+    citire();
+    for(int i = 1; i <= N; i++) {
+        preordine(v[i]);
+        if(A.size() == N) {
+            afis();
+            return 0;
+        } else
+            A.clear();
+    }
+    return 0;
+}
