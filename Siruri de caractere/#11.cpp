c6c306095c79bb729848e744365478ce1056d7b4
ShumaherK <43296487+ShumaherK@users.noreply.github.com>
2019-05-11 20:59:20 +0300

    Create #11.cpp

---
+++ Siruri de caractere/#11.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool isVow(char s) {
+    static char a[] = "aeiou";
+    for(int i = 0; i < 6; i++)
+        if(a[i] == s)
+            return 1;
+    return 0;
+}
+
+int main() {
+    char s[256];
+    cin.get(s, 255);
+    cin.get();
+    int i = 0;
+
+    while(s[i] != '\0') {
+        if(isVow(s[i]))
+            s[i]=s[i]-32;
+        cout<<s[i];
+        i++;
+    }
+
+    return 0;
+}
