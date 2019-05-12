9da9fd8a656340d0ef95f8a57efb21418e6ccdba
ShumaherK <43296487+ShumaherK@users.noreply.github.com>
2019-05-12 12:51:21 +0300

    Update #12.cpp

--- Siruri de caractere/#12.cpp
+++ Siruri de caractere/#12.cpp
@@ -1,30 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isVow(char s) {
-    static char a[] = "aeiou";
-    for(int i = 0; i < 6; i++)
-        if(a[i] == s)
-            return 1;
-    return 0;
-}
-
 int main() {
     char s[256];
     cin.get(s, 255);
     cin.get();
     char *p = strtok(s, " ");
-    while(p != NULL) {
-        cout << char(p[0] - 32);
+    while(p != NULL) { //Luam fiecare cuvant in parte
+        cout << char(p[0] - 32); //Initiala litera mare
         for(int i = 1; i < strlen(p) - 1; i++)
             cout << p[i];
         if(strlen(p) > 1)
-            cout << char(p[strlen(p) - 1] - 32) << " ";
+            cout << char(p[strlen(p) - 1] - 32) << " "; //Daca cuvantul are mai mult de o litera o transformam si pe ultima
         else
             cout << " ";
         p = strtok(NULL, " ");
     }
-
-
+    
     return 0;
 }
